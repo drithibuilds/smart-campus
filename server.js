ba46a94517b070146f5accd1b2a675b1fb8045bb
Drithi <drithiraj164@gmail.com>
2026-06-09 23:59:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -366,8 +366,10 @@ app.get("/complaints", (req, res) => {
 
 
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log("Server Running On Port 3000");
+app.listen(PORT, () => {
+   console.log(`Server running on Port ${PORT}`);
+});
 
 });
