449f28543202d643efc330df1a950198569c8bd4
Drithi <drithiraj164@gmail.com>
2026-06-10 02:33:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
-const nodemailer = require("nodemailer);
+const nodemailer = require("nodemailer");
 
 const app = express();
 
